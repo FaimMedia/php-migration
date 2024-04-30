d6bbdb95cd661007138f660d39108b6b4a412e83
Tim Peters (FaimMedia B.V.) <tim@faimmedia.nl>
2024-04-30 16:31:18 +0200

    Added migration downgrade tests

--- test/Migration/MigrationTest.php
+++ test/Migration/MigrationTest.php
@@ -53,4 +53,44 @@ class MigrationTest extends AbstractTestCase
 		parent::assertFalse(parent::tableExists('test_4'));
 		parent::assertFalse(parent::tableExists('new_table'));
 	}
+
+	/**
+	 * Test migration and downgrade to version 2
+	 */
+	public function testVersion2Downgrade(): void
+	{
+		$this->testFullMigration();
+
+		parent::assertSame($this->migrationCount(), 4);
+
+		$this->migration->run('0002');
+
+		parent::assertSame($this->migrationCount(), 3);
+
+		parent::assertFalse(parent::tableExists('new_table'));
+	}
+
+	/**
+	 * Test migration and downgrade to version 1
+	 */
+	public function testVersion1Downgrade(): void
+	{
+		$this->testVersion2Downgrade();
+
+		parent::assertSame($this->migrationCount(), 3);
+
+		$this->migration->run('0001');
+
+		parent::assertSame($this->migrationCount(), 1);
+
+		/**
+		 * Migration version 2 has empty down files, so tables should still exist
+		 */
+		parent::assertTrue(parent::tableExists('test_1'));
+		parent::assertTrue(parent::tableExists('test_2'));
+		parent::assertTrue(parent::tableExists('test_3'));
+		parent::assertTrue(parent::tableExists('test_4'));
+
+		parent::assertFalse(parent::tableExists('new_table'));
+	}
 }
